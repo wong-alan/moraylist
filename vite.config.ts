d286dac7a6c01e7dbc8881420376062e39c08f2c
wong-alan <misteralanwong@gmail.com>
2024-03-20 22:28:06 -0700

    Add rollup visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: (id) => {
+                    if (id.includes("node_modules")) {
+                        if (id.includes("@mui")) {
+                            return "vendor_mui";
+                        }
+                        return "vendor";
+                    }
+                }
+            },
+        },
+    },
+    plugins: [
+        react(),
+        svgr(),
+        visualizer({
+            template: "treemap",
+            open: true,
+            gzipSize: true,
+            brotliSize: true,
+            filename: "analyse.html",
+        })
+    ],
 })
